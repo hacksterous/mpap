51bd6c105870aec00f80be104f977a3196524caf
hacksterous <anirbax@gmail.com>
2019-06-06 19:14:35 +0530

    Update mpcomp.py
    Updated tests.

--- mpcomp.py
+++ mpcomp.py
@@ -1,29 +1,111 @@
+import random
+import mpmath.libmp
 from mpmath import *
 from mpap import mpap
-from ArbitraryPrecision import ArbitraryPrecision
-mp.dps=40
+#from ArbitraryPrecision import ArbitraryPrecision
+EPS = '1e-94'
+mp.dps=100
 
-x=mpf(100)
-y=ArbitraryPrecision(100)
-z=mpap(100)
-
-x = (x**7)/(mpf(720))
-y = (y**7)/(ArbitraryPrecision(720))
-z = (z**7)/(mpap(720))
-#print (x)
-#print (y)
-#print (z)
-print ('-----------------------------')
-
-for i in range (20):
-	x *= x/(mpf(i+1)*mpf(i+2))
-	y *= y/(ArbitraryPrecision(i+1)*ArbitraryPrecision(i+2))
-	z *= z/(mpap(i+1)*mpap(i+2))
-	#print (x)
+print ("MPMATH backend is ", mpmath.libmp.BACKEND)
+m = 101000000000000000000000
+m = random.randint(1000, m)
+r1 = random.randint(1000, m)
+badCount = 0
+for i in range (50):
+	x=mpf(r1)
+	z=mpap(r1)
+	x *= x*(-1) 
+	z *= z*(-1)#/(mpap(i+1)*mpap(i+2))
 	#print (y)
-	#print (z)
-	m = (z-mpap(str(x)))/z
-	print ("i = ", i, "mpap diff = ", m)
-	a = (y-int(x))/y
-	print ("i = ", i, "ArbPrec diff = ", a)
-	print ('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+	#print (repr(z))
+	delta = z-mpap(str(x))
+	#print ("i = ", i, "mpap diff = ", repr(delta))
+	if abs(delta) > mpap(EPS):
+		badCount += 1
+		#print ("---------------------------------------------- BAD ")
+	#print ('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+	m = max(r1, m)
+	r1 = random.randint(1000, m)
+print ("1. badCount is = ", badCount)
+
+m = random.randint(1000, m)
+r1 = random.randint(1000, m)
+r2 = random.randint(1000, m)
+badCount = 0
+for i in range (50):
+	x = mpf(r1+i+1)/mpf(r2+i+2)
+	z = mpap(r1+i+1)/mpap(r2+i+2)
+	#print (x)
+	#print (repr(z))
+	delta = z-mpap(str(x))
+	#print ("i = ", i, "mpap diff = ", repr(delta))
+	if abs(delta) > mpap(EPS):
+		badCount += 1
+		#print ("---------------------------------------------- BAD ")
+	#print ('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+	m = max(r1, m)
+	r1 = random.randint(1000, m)
+	r2 = random.randint(1000, m)
+print ("2. badCount is = ", badCount)
+
+m = random.randint(1000, m)
+r1 = random.randint(1000, m)
+r2 = random.randint(1000, m)
+badCount = 0
+for i in range (50):
+	x = (mpf(r1+i+1)/mpf(r2+i+2))**3
+	z = (mpap(r1+i+1)/mpap(r2+i+2))**3
+	#print (x)
+	#print (repr(z))
+	delta = z-mpap(str(x))
+	#print ("i = ", i, "mpap diff = ", repr(delta))
+	if abs(delta) > mpap(EPS):
+		badCount += 1
+		#print ("---------------------------------------------- BAD ")
+	#print ('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+	m = max(r1, m)
+	r1 = random.randint(1000, m)
+	r2 = random.randint(1000, m)
+print ("3. badCount is = ", badCount)
+
+badCount = 0
+for i in range (50):
+	r1 = random.randint(5001, 100000)
+	r2 = random.randint(1001, 5000)
+	a = r1/r2
+	print ("a is ", a)
+	x = mp.exp(a)
+	z = mpap(a).exp()
+	print ("x: ", x)
+	print ("z: ", repr(z))
+	delta = z-mpap(str(x))
+	#print ("i = ", i, "mpap diff = ", repr(delta))
+	if abs(delta) > mpap(EPS):
+		badCount += 1
+		print ("---------------------------------------------- BAD ")
+	#print ('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+## NOTE -- MPAP EXP matches with LibBF -- looks like MPMath
+## is not using gmpy2 backend correctly
+print ("4. EXP. badCount is = ", badCount)
+
+badCount = 0
+for i in range (50):
+	r1 = random.randint(5001, 1000000)
+	r2 = random.randint(1001, 5000)
+	a = r1/r2
+	print ("a is ", a)
+	x = mp.tan(a)
+	z = mpap(a).tan()
+	print ("x: ", x)
+	print ("z: ", repr(z))
+	delta = z-mpap(str(x))
+	#print ("i = ", i, "mpap diff = ", repr(delta))
+	if abs(delta) > mpap(EPS):
+		badCount += 1
+		print ("---------------------------------------------- BAD ")
+	#print ('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+## NOTE -- MPAP EXP matches with LibBF
+print ("4. TAN. badCount is = ", badCount)
+
